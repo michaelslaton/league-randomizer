41def00db34495fbc0895c072d4cbbd65c8e08a8
michaelslaton <michaelaaronphoto@gmail.com>
2023-05-17 00:53:07 -0700

    continued data entry

--- src/data/champions.js
+++ src/data/champions.js
@@ -1,6 +1,166 @@
 import aatrox from '../assets/champions/aatrox.png';
 import ahri from '../assets/champions/ahri.png';
 import akali from '../assets/champions/akali.png';
+import akshan from '../assets/champions/akshan.png';
+import alistar from '../assets/champions/alistar.png';
+import amumu from '../assets/champions/amumu.png';
+import anivia from '../assets/champions/anivia.png';
+import annie from '../assets/champions/annie.png';
+import aphelios from '../assets/champions/aphelios.png';
+import ashe from '../assets/champions/ashe.png';
+import aurelionsol from '../assets/champions/aurelionsol.png';
+import azir from '../assets/champions/azir.png';
+import bard from '../assets/champions/bard.png';
+import belveth from '../assets/champions/belveth.png';
+import blitzcrank from '../assets/champions/blitzcrank.png';
+import brand from '../assets/champions/brand.png';
+import braum from '../assets/champions/braum.png';
+import caitlyn from '../assets/champions/caitlyn.png';
+import camille from '../assets/champions/camille.png';
+import cassiopeia from '../assets/champions/cassiopeia.png';
+import chogath from '../assets/champions/chogath.png';
+import corki from '../assets/champions/corki.png';
+import darius from '../assets/champions/darius.png';
+import diana from '../assets/champions/diana.png';
+import draven from '../assets/champions/draven.png';
+import drmundo from '../assets/champions/drmundo.png';
+import ekko from '../assets/champions/ekko.png';
+import elise from '../assets/champions/elise.png';
+import evelynn from '../assets/champions/evelynn.png';
+import ezreal from '../assets/champions/ezreal.png';
+import fiddlesticks from '../assets/champions/fiddlesticks.png';
+import fiora from '../assets/champions/fiora.png';
+import fizz from '../assets/champions/fizz.png';
+import galio from '../assets/champions/galio.png';
+import gangplank from '../assets/champions/gangplank.png';
+import garen from '../assets/champions/garen.png';
+import gnar from '../assets/champions/gnar.png';
+import gragas from '../assets/champions/gragas.png';
+import graves from '../assets/champions/graves.png';
+import gwen from '../assets/champions/gwen.png';
+import hecarim from '../assets/champions/hecarim.png';
+import heimerdinger from '../assets/champions/heimerdinger.png';
+import illaoi from '../assets/champions/illaoi.png';
+import irelia from '../assets/champions/irelia.png';
+import ivern from '../assets/champions/ivern.png';
+import janna from '../assets/champions/janna.png';
+import jarvaniv from '../assets/champions/jarvaniv.png';
+import jax from '../assets/champions/jax.png';
+import jayce from '../assets/champions/jayce.png';
+import jhin from '../assets/champions/jhin.png';
+import jinx from '../assets/champions/jinx.png';
+import kaisa from '../assets/champions/kaisa.png';
+import kalista from '../assets/champions/kalista.png';
+import karma from '../assets/champions/karma.png';
+import karthus from '../assets/champions/karthus.png';
+import kassadin from '../assets/champions/kassadin.png';
+import katarina from '../assets/champions/katarina.png';
+import kayle from '../assets/champions/kayle.png';
+import kayn from '../assets/champions/kayn.png';
+import kennen from '../assets/champions/kennen.png';
+import khazix from '../assets/champions/khazix.png';
+import kindred from '../assets/champions/kindred.png';
+import kled from '../assets/champions/kled.png';
+import kogmaw from '../assets/champions/kogmaw.png';
+import ksante from '../assets/champions/ksante.png';
+import leblanc from '../assets/champions/leblanc.png';
+import leesin from '../assets/champions/leesin.png';
+import leona from '../assets/champions/leona.png';
+import lillia from '../assets/champions/lillia.png';
+import lissandara from '../assets/champions/lissandara.png';
+import lucian from '../assets/champions/lucian.png';
+import lulu from '../assets/champions/lulu.png';
+import lux from '../assets/champions/lux.png';
+import malphite from '../assets/champions/malphite.png';
+import malzahar from '../assets/champions/malzahar.png';
+import maokai from '../assets/champions/maokai.png';
+import masteryi from '../assets/champions/masteryi.png';
+import milio from '../assets/champions/milio.png';
+import missfortune from '../assets/champions/missfortune.png';
+import mordekaiser from '../assets/champions/mordekaiser.png';
+import morgana from '../assets/champions/morgana.png';
+import nami from '../assets/champions/nami.png';
+import nasus from '../assets/champions/nasus.png';
+import nautilus from '../assets/champions/nautilus.png';
+import neeko from '../assets/champions/neeko.png';
+import nidalee from '../assets/champions/nidalee.png';
+import nilah from '../assets/champions/nilah.png';
+import nocturne from '../assets/champions/nocturne.png';
+import nunu from '../assets/champions/nunu.png';
+import olaf from '../assets/champions/olaf.png';
+import orianna from '../assets/champions/orianna.png';
+import ornn from '../assets/champions/ornn.png';
+import pantheon from '../assets/champions/pantheon.png';
+import poppy from '../assets/champions/poppy.png';
+import pyke from '../assets/champions/pyke.png';
+import qiyana from '../assets/champions/qiyana.png';
+import quinn from '../assets/champions/quinn.png';
+import rakan from '../assets/champions/rakan.png';
+import rammus from '../assets/champions/rammus.png';
+import reksai from '../assets/champions/reksai.png';
+import rell from '../assets/champions/rell.png';
+import renataglasc from '../assets/champions/renataglasc.png';
+import renekton from '../assets/champions/renekton.png';
+import rengar from '../assets/champions/rengar.png';
+import riven from '../assets/champions/riven.png';
+import rumble from '../assets/champions/rumble.png';
+import ryze from '../assets/champions/ryze.png';
+import samira from '../assets/champions/samira.png';
+import sejuani from '../assets/champions/sejuani.png';
+import senna from '../assets/champions/senna.png';
+import seraphine from '../assets/champions/seraphine.png';
+import sett from '../assets/champions/sett.png';
+import shaco from '../assets/champions/shaco.png';
+import shen from '../assets/champions/shen.png';
+import shyvana from '../assets/champions/shyvana.png';
+import singed from '../assets/champions/singed.png';
+import sion from '../assets/champions/sion.png';
+import sivir from '../assets/champions/sivir.png';
+import skarner from '../assets/champions/skarner.png';
+import sona from '../assets/champions/sona.png';
+import soraka from '../assets/champions/soraka.png';
+import swain from '../assets/champions/swain.png';
+import sylas from '../assets/champions/sylas.png';
+import syndra from '../assets/champions/syndra.png';
+import tahm from '../assets/champions/tahm.png';
+import taliyah from '../assets/champions/taliyah.png';
+import talon from '../assets/champions/talon.png';
+import taric from '../assets/champions/taric.png';
+import teemo from '../assets/champions/teemo.png';
+import thresh from '../assets/champions/thresh.png';
+import tristana from '../assets/champions/tristana.png';
+import trundle from '../assets/champions/trundle.png';
+import tryndamere from '../assets/champions/tryndamere.png';
+import twistedfate from '../assets/champions/twistedfate.png';
+import twitch from '../assets/champions/twitch.png';
+import udyr from '../assets/champions/udyr.png';
+import urgot from '../assets/champions/urgot.png';
+import varus from '../assets/champions/varus.png';
+import vayne from '../assets/champions/vayne.png';
+import veigar from '../assets/champions/veigar.png';
+import velkoz from '../assets/champions/velkoz.png';
+import vex from '../assets/champions/vex.png';
+import vi from '../assets/champions/vi.png';
+import viego from '../assets/champions/viego.png';
+import viktor from '../assets/champions/viktor.png';
+import vladimir from '../assets/champions/vladimir.png';
+import volibear from '../assets/champions/volibear.png';
+import warwick from '../assets/champions/warwick.png';
+import wukong from '../assets/champions/wukong.png';
+import xayah from '../assets/champions/xayah.png';
+import xerath from '../assets/champions/xerath.png';
+import xinzhao from '../assets/champions/xinzhao.png';
+import yasuo from '../assets/champions/yasuo.png';
+import yone from '../assets/champions/yone.png';
+import yorick from '../assets/champions/yorick.png';
+import yuumi from '../assets/champions/yuumi.png';
+import zac from '../assets/champions/zac.png';
+import zed from '../assets/champions/zed.png';
+import zeri from '../assets/champions/zeri.png';
+import ziggs from '../assets/champions/ziggs.png';
+import zilean from '../assets/champions/zilean.png';
+import zoe from '../assets/champions/zoe.png';
+import zyra from '../assets/champions/zyra.png';
 
  const champions = [
   {
@@ -17,6 +177,291 @@ import akali from '../assets/champions/akali.png';
     id: 3,
     name: `Akali`,
     img: akali,
+  },
+  {
+    id: 4,
+    name: `Akshan`,
+    img: akshan,
+  },
+  {
+    id: 5,
+    name: `Alistar`,
+    img: alistar,
+  },
+  {
+    id: 6,
+    name: `Amumu`,
+    img: amumu,
+  },
+  {
+    id: 7,
+    name: `Anivia`,
+    img: anivia,
+  },
+  {
+    id: 8,
+    name: `Annie`,
+    img: annie,
+  },
+  {
+    id: 9,
+    name: `Aphelios`,
+    img: aphelios,
+  },
+  {
+    id: 10,
+    name: `Ashe`,
+    img: ashe,
+  },
+  {
+    id: 11,
+    name: `Aurelion Sol`,
+    img: aurelionsol,
+  },
+  {
+    id: 12,
+    name: `Azir`,
+    img: azir,
+  },
+  {
+    id: 13,
+    name: `Bard`,
+    img: bard,
+  },
+  {
+    id: 14,
+    name: `Bel'Veth`,
+    img: belveth,
+  },
+  {
+    id: 15,
+    name: `Blitzcrank`,
+    img: blitzcrank,
+  },
+  {
+    id: 16,
+    name: `Brand`,
+    img: brand,
+  },
+  {
+    id: 17,
+    name: `Braum`,
+    img: braum,
+  },
+  {
+    id: 18,
+    name: `Caitlyn`,
+    img: caitlyn,
+  },
+  {
+    id: 19,
+    name: `Camille`,
+    img: camille,
+  },
+  {
+    id: 20,
+    name: `Cassiopeia`,
+    img: cassiopeia,
+  },
+  {
+    id: 21,
+    name: `Cho'Gath`,
+    img: chogath,
+  },
+  {
+    id: 22,
+    name: `Corki`,
+    img: corki,
+  },
+  {
+    id: 23,
+    name: `Darius`,
+    img: darius,
+  },
+  {
+    id: 24,
+    name: `Diana`,
+    img: diana,
+  },
+  {
+    id: 25,
+    name: `Draven`,
+    img: draven,
+  },
+  {
+    id: 26,
+    name: `Dr.Mundo`,
+    img: drmundo,
+  },
+  {
+    id: 27,
+    name: `Ekko`,
+    img: ekko,
+  },
+  {
+    id: 28,
+    name: `Elise`,
+    img: elise,
+  },
+  {
+    id: 29,
+    name: `Evelynn`,
+    img: evelynn,
+  },
+  {
+    id: 30,
+    name: `Ezreal`,
+    img: ezreal,
+  },
+  {
+    id: 31,
+    name: `Fiddlesticks`,
+    img: fiddlesticks,
+  },
+  {
+    id: 32,
+    name: `Fiora`,
+    img: fiora,
+  },
+  {
+    id: 33,
+    name: `Fizz`,
+    img: fizz,
+  },
+  {
+    id: 34,
+    name: `Galio`,
+    img: galio,
+  },
+  {
+    id: 35,
+    name: `Gangplank`,
+    img: gangplank,
+  },
+  {
+    id: 36,
+    name: `Garen`,
+    img: garen,
+  },
+  {
+    id: 37,
+    name: `Gnar`,
+    img: gnar,
+  },
+  {
+    id: 38,
+    name: `Gragas`,
+    img: gragas,
+  },
+  {
+    id: 39,
+    name: `Graves`,
+    img: graves,
+  },
+  {
+    id: 40,
+    name: `Gwen`,
+    img: gwen,
+  },
+  {
+    id: 41,
+    name: `Hecarim`,
+    img: hecarim,
+  },
+  {
+    id: 42,
+    name: `Heimerdinger`,
+    img: heimerdinger,
+  },
+  {
+    id: 43,
+    name: `Illaoi`,
+    img: illaoi,
+  },
+  {
+    id: 44,
+    name: `Irelia`,
+    img: irelia,
+  },
+  {
+    id: 45,
+    name: `Ivern`,
+    img: ivern,
+  },
+  {
+    id: 46,
+    name: `Janna`,
+    img: janna,
+  },
+  {
+    id: 47,
+    name: `Jarvan IV`,
+    img: jarvaniv,
+  },
+  {
+    id: 48,
+    name: `Jax`,
+    img: jax,
+  },
+  {
+    id: 49,
+    name: `Jayce`,
+    img: jayce,
+  },
+  {
+    id: 50,
+    name: `Jhin`,
+    img: jhin,
+  },
+  {
+    id: 51,
+    name: `Jinx`,
+    img: jinx,
+  },
+  {
+    id: 52,
+    name: `Kai'Sa`,
+    img: kaisa,
+  },
+  {
+    id: 53,
+    name: `Kalista`,
+    img: kalista,
+  },
+  {
+    id: 54,
+    name: `Karma`,
+    img: karma,
+  },
+  {
+    id: 56,
+    name: `Karthus`,
+    img: karthus,
+  },
+  {
+    id: 57,
+    name: `Kassadin`,
+    img: kassadin,
+  },
+  {
+    id: 58,
+    name: `Katarina`,
+    img: katarina,
+  },
+  {
+    id: 59,
+    name: `Kayle`,
+    img: kayle,
+  },
+  {
+    id: 59,
+    name: `Kayn`,
+    img: kayn,
+  },
+  {
+    id: 60,
+    name: `Kennen`,
+    img: kennen,
   },
 ]
 
